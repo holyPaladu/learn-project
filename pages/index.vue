c84d4f3d3b1dfe9a67c50415aaab5142e5d6a621
holyPaladu <ironic_paladin@mail.ru>
2025-02-02 22:58:54 +0500

    learn scss full architecture, config and installing.

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="main-block"></div>
+</template>
+
+<style scoped lang="scss">
+.main-block {
+  width: 100px;
+  height: 100px;
+  background-color : $error-color;
+}
+</style>
