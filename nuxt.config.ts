6b6912db60018381536c9c1135c03d3d0c8d6913
holyPaladu <ironic_paladin@mail.ru>
2025-02-02 23:31:30 +0500

    fix: _mixins.scss error by variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: [
-    '@/assets/scss/main.scss' // Главный файл SCSS
+    '@/assets/scss/main.scss' // Main SCSS file
   ],
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
           additionalData: `
-            @use "~/assets/scss/utils/_variables.scss" as *;
-            @use "~/assets/scss/utils/_mixins.scss" as *;
+            @use "@/assets/scss/utils/_variables.scss" as *;
+            @use "@/assets/scss/utils/_mixins.scss" as *;
           `
         }
       }
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,8 @@
 </script>
 
 <template>
-<div class="main-block"></div>
+  <div class="main-block"></div>
+  <button class="button button--outline"></button>
 </template>
 
 <style scoped lang="scss">
@@ -11,5 +12,20 @@
   width: 100px;
   height: 100px;
   background-color : $error-color;
+}
+.button {
+  padding: 12px 24px;
+  border-radius: 4px;
+  background: $primary-color;
+  color: white;
+
+  @include respond-to('md') {
+    padding: 16px 32px;
+  }
+
+  &--outline {
+    background: transparent;
+    border: 2px solid $primary-color;
+  }
 }
 </style>
