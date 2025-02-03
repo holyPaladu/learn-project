f2e0e28f806094d553e861a4f4459b805b5717b9
holyPaladu <ironic_paladin@mail.ru>
2025-02-03 19:25:55 +0500

    refactor: simplify index.vue by removing unused elements and styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,5 @@
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
+<template></template>
 
-<template>
-  <div class="main-block"></div>
-  <button class="button button--outline"></button>
-</template>
-
-<style scoped lang="scss">
-.main-block {
-  width: 100px;
-  height: 100px;
-  background-color : $error-color;
-}
-.button {
-  padding: 12px 24px;
-  border-radius: 4px;
-  background: $primary-color;
-  color: white;
-
-  @include respond-to('md') {
-    padding: 16px 32px;
-  }
-
-  &--outline {
-    background: transparent;
-    border: 2px solid $primary-color;
-  }
-}
-</style>
+<style scoped lang="scss"></style>
